3650d35e23c99c2e1582f3a217c5372f2ce0c73f
maximdsouzaml <36703574+maximdsouzaml@users.noreply.github.com>
2018-06-26 19:26:04 +0530

    go

--- HousePrices-Kaggle/house_prices.py
+++ HousePrices-Kaggle/house_prices.py
@@ -3,11 +3,51 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from imports import import_func
+import seaborn as sns
 
-df = pd.read_csv("dataset/train.csv")
 
-Y=df['SalePrice']
+train = pd.read_csv("dataset/train.csv")
 
-df=df.drop(['SalePrice'], axis=1)
+##Let us check how many columns have null values
 
-empty_columns=check_empty_value_columns(df)
+train.columns[train.isnull().any()]
+
+##Let Us check the % of missing values
+missing_columns=train.isnull().sum()
+missing_columns_percentage=missing_columns/len(train)
+missing_columns_percentage=missing_columns_percentage[missing_columns_percentage>0].sort_values(ascending=False)
+
+missing_frame=missing_columns_percentage.to_frame()
+
+missing_frame.columns=['count']
+missing_frame['Name']=missing_frame.index.values
+
+sns.set(style="whitegrid", color_codes=True)
+sns.barplot(missing_frame['Name'], missing_frame['count'])
+plt.xticks(rotation = 90)
+
+
+## Let us check the distribution of the Sale Price
+
+sns.distplot(train['SalePrice'])
+
+## It is positively or right skewed. Let us try the log value distribution
+sns.distplot(np.log(train['SalePrice']))
+
+## Yes the log form is normally distributed
+
+#Let us split numeric and categorical data
+numeric=train.select_dtypes(include=[np.number])
+categoric=train.select_dtypes(exclude=[np.number])
+
+corr_matrix=numeric.corr()
+
+sns.heatmap(categoric, cmap="Blues")
+
+correlation_saleprice=corr_matrix['SalePrice'].sort_values(ascending=False)
+
+correlation_saleprice_frame=correlation_saleprice.to_frame()
+correlation_saleprice_frame['Name']=correlation_saleprice_frame.index.values
+
+sns.barplot(correlation_saleprice_frame['Name'], correlation_saleprice_frame['SalePrice'])
+plt.xticks(rotation=90)
